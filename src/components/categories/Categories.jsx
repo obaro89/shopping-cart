6b852865322abd814c32a17b237e50b873e930fd
obaro89 <igbinobaroosaretin@gmail.com>
2022-08-01 18:48:16 +0100

    category page

---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,14 @@
+import "./categories.style.scss";
+import CategoryItem from "../categoryItem/CategoryItem";
+
+const Categories = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <CategoryItem category={category} key={category.id} />
+      ))}
+    </div>
+  );
+};
+
+export default Categories;
